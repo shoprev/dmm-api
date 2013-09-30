433a803f11a68a9ff4c7e75d3b81f243d4a59c39
shoprev <admin@shoprev.net>
2013-09-30 14:19:12 +0900

    v0.1.0

--- example/com.rb
+++ example/com.rb
@@ -1,15 +1,20 @@
 require "dmm/api"
 require "yaml"
 require "pp"
+
 config = YAML.load_file(File.dirname(__FILE__)+'/config.yml')
+
 Dmm::Api.configure do |options|
   options[:api_id] = config["api_id"]
   options[:affiliate_id] = config["affiliate_id"]
   options[:site] = "DMM.com"
 end
+
 res = Dmm::Api.get({:service => 'lod',:floor => 'nmb48',:keyword => "山本彩",:hits => "1"})
-p res["result"]["result_count"]
-# p res[:result][:items][:item]
-# res["result"]["items"]["item"].each do |v|
-#   pp v #["title"]
-# end
+pp res["result"]["result_count"]
+pp res["result"]["items"]["item"]["title"]
+
+res = Dmm::Api.get({:service => 'lod',:floor => 'nmb48',:keyword => "山本彩"})
+res["result"]["items"]["item"].each do |v|
+  pp v["title"]
+end
